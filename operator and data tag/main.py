ce49039b73021a2a3520a0286e481854dbbcd4b7
kaan5325 <kaanvardar000@gmail.com>
2024-09-30 18:47:22 +0400

    operators

---
+++ operator and data tag/main.py
@@ -0,0 +1,18 @@
+name = "kaan"
+age =  10
+is_student = True
+weight = 36.26
+
+print("name:",name)
+print("age:",age)
+print("is student:",is_student)
+print("weight:",weight)
+
+print("data type of name is:",type(name))
+print("data type of age is:",type(age))
+print("data type of is student is:",type(is_student))
+print("data type of weight is:",type(weight))
+
+weight=int(weight)
+print(weight)
+print("data type of weight",type(weight))
---
+++ operator and data tag/operator.py
@@ -0,0 +1,11 @@
+n1 = 62
+n2 = 7
+
+print("addition",n1+n2)
+print("subtraction",n1-n2)
+print("multiplication",n1*n2)
+print("division",n1/n2)
+print("floor division",n1//n2)
+print("modulus",n1%n2)
+print("square",n1**2)
+print("square root",n1**0.5)
