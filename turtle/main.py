94bbc3b69b33611171cdf2b8d72a34c6d6c5d7ae
kaan5325 <kaanvardar000@gmail.com>
2024-10-03 18:41:32 +0400

    turtle

---
+++ turtle/main.py
@@ -0,0 +1,6 @@
+import turtle
+turtle.Screen().bgcolor("Orange")
+a = turtle.Screen()
+a.setup(400,300)
+turtle.title("welcome")
+turtle.done()
---
+++ turtle/main1.py
@@ -0,0 +1,26 @@
+import turtle
+turtle.Screen().bgcolor("Orange")
+c = turtle.Turtle()
+c.forward(120)
+c.left(120)
+
+c.forward(120)
+c.left(120)
+
+c.forward(120)
+c.left(120)
+
+c.forward(120)
+c.right(90)
+
+c.forward(120)
+c.right(90)
+
+c.forward(120)
+c.right(90)
+
+c.forward(120)
+c.right(90)
+turtle.done()
+
+
