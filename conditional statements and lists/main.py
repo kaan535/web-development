9f340a2048d69167ff7582419a9085ec231b74a1
kaan5325 <kaanvardar000@gmail.com>
2024-10-07 18:45:13 +0400

    statement, list

---
+++ conditional statements and lists/main.py
@@ -0,0 +1,11 @@
+number = int(input("enter a number"))
+
+print("number to be checked",number)
+
+if number<0:
+    print("negative")
+elif number>0:
+ 
+ print("positive")
+else:
+   print("nuetral")
