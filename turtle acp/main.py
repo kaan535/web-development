3537ead9b105d06e6b5b2f685344c5ae3d22b62e
kaan5325 <kaanvardar000@gmail.com>
2024-10-09 19:22:31 +0400

    draw things with turtle

---
+++ turtle acp/main.py
@@ -0,0 +1,26 @@
+import turtle
+turtle.Screen().bgcolor("red")
+c = turtle.Turtle()
+c.forward(130)
+c.left(130)
+
+c.forward(130)
+c.left(130)
+
+c.forward(130)
+c.left(130)
+
+c.forward(130)
+c.right(80)
+
+c.forward(130)
+c.right(80)
+
+c.forward(130)
+c.right(80)
+
+c.forward(130)
+c.right(80)
+
+c.forward(100)
+turtle.done()
