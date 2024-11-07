ea95b0ee9f33753a8e7da582071b06a8abe569e1
kaan5325 <kaanvardar000@gmail.com>
2024-11-07 17:15:19 +0400

    dictionarys & tuples

---
+++ dictionarys & tuples/main.py
@@ -0,0 +1,14 @@
+my_dict = {'name':'kaan', 'age':10}
+
+print(my_dict['name'])
+print(my_dict['age'])
+print(my_dict)
+my_dict['age']=10.9
+
+print(my_dict)
+
+my_dict['adress']='dubai'
+
+print(my_dict)
+my_dict.clear()
+print(my_dict)
